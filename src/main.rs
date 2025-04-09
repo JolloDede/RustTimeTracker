29fe9ed7e6bca72e5c87c0e0fc5242387a5abd54
Dennis Lee <dl@aduna.ch>
2025-04-09 14:57:01 +0200

    feat: router add

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,89 @@
-use dioxus::prelude::*;
+use dioxus::{
+    html::{div, header},
+    prelude::*,
+};
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
-const HEADER_SVG: Asset = asset!("/assets/header.svg");
 const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
+const HOME_SVG: Asset = asset!("/assets/home.svg");
+const SETTINGS_SVG: Asset = asset!("/assets/settings.svg");
+const ARROW_SVG: Asset = asset!("/assets/backarrow.svg");
+const OPTION_SVG: Asset = asset!("/assets/options.svg");
+
+
 fn main() {
-    dioxus::launch(App);
+    dioxus::launch(|| {
+        rsx! {
+            document::Link { rel: "icon", href: FAVICON }
+            document::Link { rel: "stylesheet", href: TAILWIND_CSS }
+            Router::<Route> {}
+        }
+    });
+}
+
+#[derive(Routable, PartialEq, Clone)]
+enum Route {
+    #[route("/")]
+    App {},
 }
 
 #[component]
 fn App() -> Element {
     rsx! {
-        document::Link { rel: "icon", href: FAVICON }
-        document::Link { rel: "stylesheet", href: TAILWIND_CSS }
-        Hero {}
+        div {
+            class: "flex flex-col h-screen justify-between",
+            header {                
+                div {
+                    class: "border-b flex justify-between h-10",
+                    div {
+                        class: "w-8 pt-1.5 rounded-full ml-2 hover:bg-gray-200",
+                        img { src: ARROW_SVG }
+                    },
+                    div {
+                        // "Title"
+                    },
+                    div {
+                        class: "w-7 pt-1.5 rounded-full hover:bg-gray-200 mr-2",
+                        img { src: OPTION_SVG }
+                    }
+                }
+            },
+            main {
+                class: "flex-grow p-4 overflow-y-auto",
+                Card { "Default" }
+             },
+            MainFooter {}
+         }
+    }
+}
 
+
+#[component]
+fn MainFooter() -> Element {
+    rsx! {
+        footer {
+            div {
+                class: "flex justify-around border-t py-2 ",
+                div {
+                    class: "w-12 border rounded-full p-2 hover:bg-gray-200",
+                    img { src: HOME_SVG, class: ""}
+                },
+                div {
+                    class: "w-12 border rounded-full p-2 hover:bg-gray-200",
+                    img { src: SETTINGS_SVG, class: ""}
+                }
+            }
+         }
     }
 }
 
 #[component]
-pub fn Hero() -> Element {
+fn Card(children: Element) -> Element {
     rsx! {
         div {
-            id: "hero",
-            // img { src: HEADER_SVG, id: "header" }
-            // div { id: "links",
-            //     a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
-            //     a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-            //     a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-            //     a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-            //     a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-            //     a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
-            // }
+            class: "flex border rounded-lg px-4 py-2",
+            {children}
         }
     }
 }
